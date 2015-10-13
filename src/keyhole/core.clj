9e51dcdda257c65a4ac023401cfd6c6428cd9c0e
Lars Andersen <lars.andersen@gjensidige.no>
2015-10-13 14:02:28 +0200

    New approach

--- src/keyhole/core.clj
+++ src/keyhole/core.clj
@@ -1,5 +1,5 @@
 (ns keyhole.core
-  (:refer-clojure :rename {filter cfilter range crange}))
+  (:require [clojure.walk :as walk]))
 
 (defprotocol TypePreservingMapper
   (map* [coll f] "Map f over coll, preserving coll's type."))
@@ -70,55 +70,107 @@
   clojure.lang.APersistentSet
   (map* [v f] (into #{} (map f v))))
 
-(defrecord RangeSelector [start end step]
-  Selector
-  (select* [this next-fn xs]
-    (map next-fn (some->> xs (drop start) (take (- end start)) (take-nth step))))
-  Transformer
-  (transform* [this next-fn xs]
-    (map* (some->> xs (drop start) (take (- end start)) (take-nth step))
-          next-fn)))
+(defprotocol Transformer
+  (emit [this] "Emit transformer code."))
 
-(defn ->RangeSelector [[_ start end step]]
-  (RangeSelector. start end (or step 1)))
+(extend-protocol Transformer
+
+  clojure.lang.Keyword
+  (emit [this] `(partial update* ::next-fn ~this)))
+
+(defrecord RangeSpec [start end step]
+  Transformer
+  (emit [this] `(partial update-seq ::next-fn ~start ~end ~step)))
+
+(defn make-RangeSpec [[_ start end step]]
+  (RangeSpec. start end (or step 1)))
 
 (defn parse-filter [f]
   (cond
-    (range-spec? f) (->RangeSelector f)
+    (range-spec? f) (make-RangeSpec f)
     :else f))
 
 (defn- parse-filter-spec [spec]
   (map parse-filter spec))
 
-(defmacro select [coll spec]
-  (let [spec (partition 2 2 [identity] (parse-filter-spec spec))]
-    `(some->> ~coll
-              ~@(for [[s1 s2] spec]
-                  `(select* ~s1 ~s2)))))
+;; (transform [{:foo [1 2 3]} {:foo [4 5 6]} {:foo [7 8 9]} {:foo [10 11 12]}]
+;;            [(range 0 2) :foo (range 2 3)] inc)
+;; (select  [{:foo 1} {:foo 2} {:foo 3} {:foo 4}] [(range 0 2) :foo])
 
-(defn- pick-transformer [spec]
-  (if (some (some-fn (partial instance? RangeSelector)) spec)
-    ->SeqTransformer
-    ->ValTransformer))
+(defn- slice
+  "Extract the elements between start and end (exclusive) by step.
+
+  (slice [1 2 3 4] 0 3 2) => [1 3]
+
+  The type of xs is preserved."
+  [xs start end step]
+  (let [xs' (some->> xs (drop start) (take (- end start)) (take-nth step))]
+    (if (vector? xs)
+      (into [] xs')
+      xs')))
+
+(defmacro do1
+  "Like do but return the value of the first form instead of the
+  last."
+  [res & exprs*]
+  `(let [res# ~res]
+     `(do ~~@exprs*)
+     res#))
+
+(defn- splice
+  "Splice the elements of ys into xs by replacing every step element
+  between start ane end (exclusive).
+
+  Splice is the reverse of slice.
+
+  (splice [1 2 3 4] (map (constantly 0) (slice [1 2 3 4])) 0 3 2) => [0 2 0 4]
+
+  The type of the result is that of xs."
+  [xs ys start end step]
+  (let [xs' (into [] xs)
+        ys (into [] ys)
+        yi (volatile! 0)
+        res (for [i (range (count xs'))]
+              (if (and (>= i start)
+                       (< i end)
+                       (= (rem i step) 0))
+                (do1 (nth ys @yi) (vswap! yi inc))
+                (nth xs' i)))]
+    (if (vector? xs)
+      (into [] res)
+      res)))
+
+(defn- update-seq
+  "Apply f to every step element of xs between start and end (exclusive.)"
+  [f start end step xs]
+  (splice xs (map f (slice xs start end step)) start end step))
+
+(defn update* [f k m]
+  (let [m' (update {} k (constantly (f (get m k))))]
+    (merge m m')))
+
+(defn- combine [forms form]
+  (walk/postwalk (fn [f] (if (= f ::next-fn) form f)) forms))
+
+(defn- make-transformer [coll spec transformer]
+  (let [forms (reverse (conj (map emit spec) transformer))]
+    (reduce combine (list (first forms) coll)
+            (rest forms))))
 
 (defmacro transform [coll spec transformer]
-  (let [spec (reverse (partition 2 1 [identity] (parse-filter-spec spec)))
-        transformer ((pick-transformer spec) (eval transformer))]
-    `(some->> (some->> ~coll ~@(for [[s1 s2] spec]
-                                 `(select* ~s1 ~s2)))
-              ~@(for [[s1 s2] (conj spec [transformer identity])]
-                  `(transform* ~s1 ~s2)))))
-
-(transform [{:foo 1} {:foo 2} {:foo 3} {:foo 4}] [(range 0 2)] #(assoc % :bar 2))
-;; (select  [{:foo 1} {:foo 2} {:foo 3} {:foo 4}] [(range 0 2) :foo])
+  (let [spec (reverse (parse-filter-spec spec))]
+    (make-transformer coll spec (eval transformer))))
 
 (defn benchmark [iters afn]
   (time
    (dotimes [_ iters]
      (afn))))
 
-(def DATA {:a {:b {:c 1}}})
+(transform [{:foo [1 2 3]} {:foo [4 5 6]} {:foo [7 8 9]} {:foo [10 11 12]}]
+           [(range 0 2) :foo (range 2 3)] inc)
 
+(def DATA {:a {:b {:c 1}}})
+;; (transform DATA [:a :b :c] inc )
 ;; (benchmark 1000000 #(get-in DATA [:a :b :c]))
 ;; ;; => "Elapsed time: 77.018 msecs"
 
@@ -128,20 +180,20 @@
 ;; (benchmark 1000000 #(-> DATA :a :b :c vector))
 ;; ;; => "Elapsed time: 34.235 msecs"
 
-(benchmark 1000000 #(update-in DATA [:a :b :c] inc))
+;; (benchmark 1000000 #(update-in DATA [:a :b :c] inc))
 ;; => "Elapsed time: 1037.94 msecs"
 
-(benchmark 1000000 #(transform DATA [:a :b :c] inc ))
+;; (benchmark 1000000 #(transform DATA [:a :b :c] inc))
 ;; => "Elapsed time: 4305.429 msecs"
-(transform DATA [:a :b :c] inc )
-(defn manual-transform [data]
-  (update data
-          :a
-          (fn [d1]
-            (update d1
-                    :b
-                    (fn [d2]
-                      (update d2 :c inc))))))
+;; (transform DATA [:a :b :c] inc )
+;; (defn manual-transform [data]
+;;   (update data
+;;           :a
+;;           (fn [d1]
+;;             (update d1
+;;                     :b
+;;                     (fn [d2]
+;;                       (update d2 :c inc))))))
 
-(benchmark 1000000 #(manual-transform DATA))
+;; (benchmark 1000000 #(manual-transform DATA))
 ;; => "Elapsed time: 161.945 msecs"
