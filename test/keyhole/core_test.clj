792ea2563c76380f96f0ec20d6eb7aa00c2e7dfc
Lars Andersen <expez@expez.com>
2015-10-15 15:25:49 +0200

    Dry off tests slightly

--- test/keyhole/core_test.clj
+++ test/keyhole/core_test.clj
@@ -2,42 +2,48 @@
   (:require [clojure.test :as t]
             [keyhole.core :as k]))
 
-(t/deftest keyword-test
-  (t/is (= 1 (k/select {:foo {:bar 1}} [:foo :bar])))
-  (t/is (= {:foo {:bar 2}}
-           (k/transform {:foo {:bar 1}} [:foo :bar] inc))))
+(defmacro defexamples [name & expected-actual-pairs]
+  (when-not (even? (count expected-actual-pairs))
+    (throw (ex-info "defexamples requires an even number of expected-actual-pairs"
+                    {:expected-actual-pairs expected-actual-pairs})))
+  `(t/deftest ~name
+     ~@(for [[expected actual] (partition 2 expected-actual-pairs)]
+         `(t/is (= ~expected ~actual)))))
 
-(t/deftest range-test
-  (t/is (= [0 1] (k/select [0 1 2] [(range 0 2)])))
-  (t/is (= [0 1 2] (k/transform [-1 0 2] [(range 0 2)] inc))))
+(defexamples keyword-test
+  1 (k/select {:foo {:bar 1}} [:foo :bar])
+  {:foo {:bar 2}} (k/transform {:foo {:bar 1}} [:foo :bar] inc))
 
-(t/deftest all*-test
-  (t/is (= [0 1 2] (k/select [{:foo 0} {:foo 1} {:foo 2}] [all* :foo])))
-  (t/is (= [{:foo 1} {:foo 2} {:foo 3}]
-           (k/transform [{:foo 0} {:foo 1} {:foo 2}] [all* :foo] inc))))
+(defexamples range-test
+  [0 1] (k/select [0 1 2] [(range 0 2)])
+  [0 1 2] (k/transform [-1 0 2] [(range 0 2)] inc))
 
-(t/deftest first*-test
-  (t/is (= 0 (k/select [{:foo 0} {:foo 1} {:foo 2}] [first* :foo])))
-  (t/is (= [{:foo 1} {:foo 1} {:foo 2}]
-           (k/transform [{:foo 0} {:foo 1} {:foo 2}] [first* :foo] inc))))
+(defexamples all*-test
+  [0 1 2] (k/select [{:foo 0} {:foo 1} {:foo 2}] [all* :foo])
+  [{:foo 1} {:foo 2} {:foo 3}]
+  (k/transform [{:foo 0} {:foo 1} {:foo 2}] [all* :foo] inc))
 
-(t/deftest last*-test
-  (t/is (= 2 (k/select [{:foo 0} {:foo 1} {:foo 2}] [last* :foo])))
-  (t/is (= [{:foo 0} {:foo 1} {:foo 3}]
-           (k/transform [{:foo 0} {:foo 1} {:foo 2}] [last* :foo] inc))))
+(defexamples first*-test
+  0 (k/select [{:foo 0} {:foo 1} {:foo 2}] [first* :foo])
+  [{:foo 1} {:foo 1} {:foo 2}]
+  (k/transform [{:foo 0} {:foo 1} {:foo 2}] [first* :foo] inc))
 
-(t/deftest butlast*-test
-  (t/is (= [0 1] (k/select [{:foo 0} {:foo 1} {:foo 2}] [butlast* :foo])))
-  (t/is (= [{:foo 1} {:foo 2} {:foo 2}]
-           (k/transform [{:foo 0} {:foo 1} {:foo 2}] [butlast* :foo] inc))))
+(defexamples last*-test
+  2 (k/select [{:foo 0} {:foo 1} {:foo 2}] [last* :foo])
+  [{:foo 0} {:foo 1} {:foo 3}]
+  (k/transform [{:foo 0} {:foo 1} {:foo 2}] [last* :foo] inc))
 
-(t/deftest rest*-test
-  (t/is (= [1 2] (k/select [{:foo 0} {:foo 1} {:foo 2}] [rest* :foo])))
-  (t/is (= [{:foo 0} {:foo 2} {:foo 3}]
-           (k/transform [{:foo 0} {:foo 1} {:foo 2}] [rest* :foo] inc))))
+(defexamples butlast*-test
+  [0 1] (k/select [{:foo 0} {:foo 1} {:foo 2}] [butlast* :foo])
+  [{:foo 1} {:foo 2} {:foo 2}]
+  (k/transform [{:foo 0} {:foo 1} {:foo 2}] [butlast* :foo] inc))
 
-(t/deftest pred-test
-  (t/is (= [2 4]
-           (k/select [{:foo 1} {:foo 2} {:foo 4}] [all* :foo even?])))
-  (t/is (= [{:foo 1} {:foo 1} {:foo 3}]
-           (k/transform [{:foo 1} {:foo 2} {:foo 4}] [all* :foo even?] dec))))
+(defexamples rest*-test
+  [1 2] (k/select [{:foo 0} {:foo 1} {:foo 2}] [rest* :foo])
+  [{:foo 0} {:foo 2} {:foo 3}]
+  (k/transform [{:foo 0} {:foo 1} {:foo 2}] [rest* :foo] inc))
+
+(defexamples pred-test
+  [2 4] (k/select [{:foo 1} {:foo 2} {:foo 4}] [all* :foo even?])
+  [{:foo 1} {:foo 1} {:foo 3}]
+  (k/transform [{:foo 1} {:foo 2} {:foo 4}] [all* :foo even?] dec))
