ab3425c6373456f165df26a3f6c789752fcb9896
Lars Andersen <expez@expez.com>
2015-10-22 14:49:19 +0200

    Get rid of placeholders in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject keyhole "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Keyhole surgery on data."
+  :url "https://github.com/expez/keyhole"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[com.rpl/specter "0.8.0"]]}})
