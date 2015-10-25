20c0cc49b7bac0a83c320f14cc75968e1a264592
Lars Andersen <expez@expez.com>
2015-10-25 14:51:38 +0100

    Move clojure dep into dev profile

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject keyhole "0.1.0-SNAPSHOT"
-  :description "Keyhole surgery on data."
+  :description "Perform keyhole surgery on data with ease"
   :url "https://github.com/expez/keyhole"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:dependencies [[com.rpl/specter "0.8.0"]]}})
+  :profiles {:dev {:dependencies [[com.rpl/specter "0.8.0"]
+                                  [org.clojure/clojure "1.7.0"]]}})
